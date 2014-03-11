155586adceca77f0b964f9c39eaa3038948b77f0
ruanyl <ruanyu1@gmail.com>
2014-03-12 00:24:30 +0200

    init commit

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['Gruntfile.js', 'src/*.js']
+    },
+    watch: {
+      files: ['src/*.js'],
+      tasks: ['jshint']
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: '.'
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('default', ['connect', 'watch']);
+};
